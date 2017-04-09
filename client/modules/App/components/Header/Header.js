a52efbdea6a779a1ae3a1410ecaa1d5f029fd911
zivkaziv <zivkalderon95@gmail.com>
2017-04-10 00:16:26 +0300

    fix package json error

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -1,12 +1,11 @@
 import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
 
 // Import Style
 import styles from './Header.css';
-import BoolLogoSvg from "../../IconSvg";
+import BoolLogoSvg from '../../IconSvg';
 
-export function Header(props, context) {
+export function Header(props) {
   const languageNodes = props.intl.enabledLanguages.map(
     lang => <li key={lang} onClick={() => props.switchLanguage(lang)} className={lang === props.intl.locale ? styles.selected : ''}>{lang}</li>
   );
@@ -20,16 +19,6 @@ export function Header(props, context) {
           {languageNodes}
         </ul>
       </div>
-      {/*<div className={styles.content}>*/}
-        {/*<h1 className={styles['site-title']}>*/}
-          {/*<Link to="/" ><FormattedMessage id="siteTitle" /></Link>*/}
-        {/*</h1>*/}
-        {/*{*/}
-          {/*context.router.isActive('/', true)*/}
-            {/*? <a className={styles['add-post-button']} href="#" onClick={props.toggleAddPost}><FormattedMessage id="addPost" /></a>*/}
-            {/*: null*/}
-        {/*}*/}
-      {/*</div>*/}
     </div>
   );
 }
