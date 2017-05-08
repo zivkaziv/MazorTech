5edf62a5c92c23d37124c6330a5bdba3e7feacbc
Yoav <yoav1357@gmail.com>
2017-05-08 22:49:14 +0300

    .

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
   mongoURL: 'mongodb://heroku_8tlqs9j6:ic64tk953vbvp6l9j20ftjinio@ds157380.mlab.com:57380/heroku_8tlqs9j6' || 'mongodb://localhost:27017/mern-starter',
-  //Test1234
+  //Test12345
   // mongoURL: process.env.NODE_ENV === 'production'? 'mongodb://heroku_8tlqs9j6:ic64tk953vbvp6l9j20ftjinio@ds157380.mlab.com:57380/heroku_8tlqs9j6' : 'mongodb://localhost:27017/mern-starter',
   port: process.env.PORT || 8000,
 };
